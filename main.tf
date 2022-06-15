1737c4c4600380bc812fa7e82933e48ebba088ec
Israel Dias <israeldiasvieira@live.com>
2022-06-15 19:22:16 -0300

    update

--- main.tf
+++ main.tf
@@ -14,7 +14,13 @@ provider "google" {
 }
 
 module "instances" {
-
   source     = "./modules/instances"
+}
 
+module "storage" {
+  source     = "./modules/storage"
+}
+
+module "network" {
+  source     = "./modules/network"
 }
